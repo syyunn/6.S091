bc6ae22cafbd7ceee7e9758382fed0fc40542d46
Suyeol Yun <syyun@mit.edu>
2023-01-23 22:45:22 -0500

    add all

---
+++ pset2/code/problem1.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+from pathlib import Path
+print("File      Path:", Path(__file__).absolute())
+
+X = pd.read_csv('pset2/code/nongaussian_samples.csv',delimiter=' ', header=None, index_col=False)
+X.columns = ['X1', 'X2']
+
+from sklearn.linear_model import LinearRegression
+reg1 = LinearRegression().fit(X[['X1']], X[['X2']])
+b_12 = reg1.coef_[0][0]
+print("b12:", b_12)
+
+reg2 = LinearRegression().fit(X[['X2']], X[['X1']])
+b_21 = reg2.coef_[0][0]
+print("b21:", b_21)
+import matplotlib.pyplot as plt
+
+plt.scatter(X['X1'], X['X2'] - b_12 * X['X1'], label='(a)')
+plt.scatter(X['X2'], X['X1'] - b_21 * X['X2'], label='(b)')
+plt.legend()
+plt.savefig('pset2/code/problem1.png')
+
+# plt.show()
+
+if __name__ == '__main__':
+    pass
+
