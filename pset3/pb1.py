bf831fcb47865f52f986e53c694a377b303c2cd7
Suyeol Yun <syyun@mit.edu>
2023-02-01 20:21:23 -0500

    add all

--- pset3/pb1.py
+++ pset3/pb1.py
@@ -4,17 +4,14 @@ import networkx as nx
 import itertools
 import matplotlib.pyplot as plt
 
-from utils import compute_partial_correlation, relabel_nodes
+from utils import compute_pvalue, relabel_nodes
 
-# read imap_samples.csv with numpy
 samples = np.genfromtxt('pset3/imap_samples.csv', delimiter=' ')
 
 
 def minimal_map(samples, perm, alpha):
     G = nx.DiGraph()
     G.add_nodes_from(perm)
-
-    # get all permutations of 2 nodes in perm
 
     comb = list(itertools.combinations(perm, 2))
     for n1, n2 in comb:
@@ -24,12 +21,15 @@ def minimal_map(samples, perm, alpha):
         else:
             i = n2
             j = n1
-
+        print("i,j:", i, j)
         pre_j = [e for e in perm if perm.index(e) < perm.index(j)]
         pre_j.remove(i)
+        print("pre_j:", pre_j)
+
         S = pre_j
 
-        if compute_partial_correlation(samples, i, j, S) < alpha: # reject the null
+        if compute_pvalue(samples, i, j, S) < alpha: 
+            # reject the null, i.e. add edge i->j
             G.add_edge(i, j)
 
     return G
@@ -37,9 +37,25 @@ def minimal_map(samples, perm, alpha):
 perm_a = [0, 1, 2, 3, 4]
 G_a = minimal_map(samples, perm_a, 0.05)
 G_a = relabel_nodes(G_a, perm_a) # add 1 to nodes
-nx.draw(G_a, with_labels=True)
-plt.show()
 
+print("G_a.edges:", len(G_a.edges))
+nx.draw(G_a, with_labels=True)
+plt.savefig('pset3/tex/images/pb1a.png')
+plt.close()
+
+perm_b = [4, 3, 0, 1, 2]
+G_b = minimal_map(samples, perm_b, 0.05)
+G_b = relabel_nodes(G_b, perm_b) # add 1 to nodes
+nx.draw(G_b, with_labels=True)
+plt.savefig('pset3/tex/images/pb1b.png')
+plt.close()
+
+perm_c = [4, 3, 2, 1, 0]
+G_c = minimal_map(samples, perm_c, 0.05)
+G_c = relabel_nodes(G_c, perm_c) # add 1 to nodes
+nx.draw(G_c, with_labels=True)
+plt.savefig('pset3/tex/images/pb1c.png')
+plt.close()
 
 if __name__ == "__main__":
     pass
--- pset3/search_mec.py
+++ pset3/search_mec.py
@@ -1,10 +1,22 @@
 import numpy as np
 import networkx as nx
 
+from matplotlib import pyplot as plt
 
-def covered_edge_neighbors(dag):
-    # TODO
-    pass
+
+def covered_edge_neighbors(G):
+    cvrd_edgs = []
+    edgs = list(G.edges)
+    for i, j in edgs:
+        print("i,j: ", i, j)
+        an_i = nx.ancestors(G, i)
+        an_i.add(i)
+        an_j = nx.ancestors(G, j)
+        if an_j == an_i:
+            print(an_i, an_j)
+            cvrd_edgs.append((i, j))
+    print("cvrd_edgs: ", cvrd_edgs)
+    return cvrd_edgs
 
 
 def search_mec(starting_dag, ending_dag):
@@ -17,17 +29,20 @@ if __name__ == "__main__":
 
     examples = []
     for ix in range(num_examples):
-        a1 = np.loadtxt(f"mec_examples/starting_dag{ix}.csv")
-        a2 = np.loadtxt(f"mec_examples/ending_dag{ix}.csv")
+        a1 = np.loadtxt(f"pset3/mec_examples/starting_dag{ix}.csv")
+        a2 = np.loadtxt(f"pset3/mec_examples/ending_dag{ix}.csv")
         starting_dag = nx.from_numpy_array(a1, create_using=nx.DiGraph)
         ending_dag = nx.from_numpy_array(a2, create_using=nx.DiGraph)
         examples.append((starting_dag, ending_dag))
 
     # PART A
     for starting_dag, _ in examples:
+        print(starting_dag)
         num_neighbors = len(covered_edge_neighbors(starting_dag))
+        print(num_neighbors)
 
     # PART B
     for starting_dag, ending_dag in examples:
-        path = search_mec(starting_dag, ending_dag)
-        print(f"Length of path: {len(path)}")
+        pass
+        # path = search_mec(starting_dag, ending_dag)
+        # print(f"Length of path: {len(path)}")
